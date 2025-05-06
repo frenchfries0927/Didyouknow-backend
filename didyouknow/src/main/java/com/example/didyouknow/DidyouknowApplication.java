ab40628c12587da8194ea43adf25015b9e34bcbd
sangyeop123 <saddleonwind@gmail.com>
2025-05-06 15:53:37 +0900

    init: backend with Spring Boot

---
+++ didyouknow/src/main/java/com/example/didyouknow/DidyouknowApplication.java
@@ -0,0 +1,13 @@
+package com.example.didyouknow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DidyouknowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DidyouknowApplication.class, args);
+    }
+
+}
---
+++ didyouknow/src/test/java/com/example/didyouknow/DidyouknowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.didyouknow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DidyouknowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
